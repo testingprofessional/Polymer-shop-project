50f143a13fc7ffa72df1d3a60c1b28ceb16384be
testingprofessional <loeffen19@hotmail.com>
2016-10-28 12:15:21 +0200

    Add files via upload

---
+++ Constant.java
@@ -0,0 +1,17 @@
+
+public class Constant {
+	
+	/***************************************
+	 * Storage place for constant variables
+	 **************************************/
+	
+	//Main URL
+	public static final String mainUrl = "https://shop.polymer-project.org";
+	
+	// Amount of men's Outerwear Items on page
+	public static final int mensOuterwearItems = 16;
+	
+	// Amount of ladies Outerwear Items on page
+	public static final int ladiesOuterwearItems = 6;
+
+}
